41425e531621159f41fa3af7bd98a9cef2bb4bdf
bobbymore2025 <88192206+bobbymore2025@users.noreply.github.com>
2021-12-13 17:46:53 +0530

    Update show.controller.js

--- movies/src/controllers/show.controller.js
+++ movies/src/controllers/show.controller.js
@@ -6,13 +6,25 @@ const Show = require("../models/show.model");
 
 const router = express.Router();
 
+router.post("/",  async (req, res) => {
+  try {
+    const user = req.user;
+
+    const movie = await Movie.create(req.body);
+
+    return res.status(201).json({ movie });
+  } 
+  catch (e) {
+    return res.status(500).json({ status: "failed", message: e.message });
+  }
+});
 
 
 router.get("/", async (req, res) => {
     const Movieid = req.query.movie_id || "titanic";
-  const movies = await Movie.find({ movies_ids: Movieid}).lean().exec();
+  const shows = await Show.find({ movies_ids: Movieid}).lean().exec();
 
-  return res.send(movies);
+  return res.send(shows);
 });
 
 module.exports = router;
